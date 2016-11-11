e3248a30ac028568500d683f75e5a9475d9fb26d
xiaxiaodong <441658311@qq.com>
2016-11-11 17:05:58 +0800

    添加复制的一份closure库,保证代码可以直接运行

---
+++ closure-library/start.js
@@ -0,0 +1,10 @@
+goog.require('goog.dom');
+
+myproject.start = function() {
+  var newDiv = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
+    'Hello world!');
+  goog.dom.appendChild(document.body, newDiv);
+};
+
+// Ensures the symbol will be visible after compiler renaming.
+goog.exportSymbol('myproject.start', myproject.start);
